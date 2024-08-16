86c1c4db53a5bdf61fde03289e77f65fd711f848
Dongsu Jang <77693+iolo@users.noreply.github.com>
2024-08-16 23:09:18 +0900

    goodbye jekyll

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  site: {
+    url: 'https://iolo.kr',
+    title: 'Quest of Iolo',
+    description: "Ho eyo he hum! Here the Jesters sing Ho Ho, But why they do, I\'ll never know.",
+    image: '/assets/img/iolo50.png',
+  },
+};
--- posts.md
+++
@@ -1,6 +0,0 @@
----
-title: Posts
----
-{% for post in site.posts %}
-* [{{ post.title }}]({{ post.url }}) <small>{{ post.date | date_to_string }}</small>
-{% endfor %}
